9746486f7f971f929d3c6e8165b8f37d9d2a6a61
Daniel <daniel.wuck@gmail.com>
2011-06-29 20:14:21 +0200

    Did a generate.

---
+++ test/factories/posts.rb
@@ -0,0 +1,5 @@
+Factory.define :post, :class => Post do |f|
+  f.name "Hello"
+  f.title "Hello"
+  f.description "Lorem ipsum dolor sit amet..."
+end
---
+++ test/unit/post_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class PostTest < ActiveRecord::TestCase
+  
+  setup do
+    
+  end
+  
+  test 'something' do
+    assert true
+  end
+  
+end
