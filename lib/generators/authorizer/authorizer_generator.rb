185f598f676e7c8ee51f6d0a023c3f461cef4fea
Daniel <daniel.wuck@gmail.com>
2011-07-01 15:54:15 +0200

    Going to say FU to generator.

--- lib/generators/authorizer/authorizer_generator.rb
+++ lib/generators/authorizer/authorizer_generator.rb
@@ -1,35 +1,18 @@
 class AuthorizerGenerator < Rails::Generator::Base
   def manifest
+    file_name = generate_file_name
+    @migration_name = file_name.camelize
     record do |m|
-      m.migration_template 'migration:migration.rb', "db/migrate", {:assigns => my_assigns,
-        :migration_file_name => filename
-      }
+      m.migration_template "authorizer_migration.rb.erb",
+                           File.join('db', 'migrate'),
+                           :migration_file_name => file_name
     end
   end
 
   private
-  #  def custom_file_name
-  #    custom_name = class_name.underscore.downcase
-  #    custom_name = custom_name.pluralize if ActiveRecord::Base.pluralize_table_names
-  #  end
 
-  def filename
+  def generate_file_name
     "create_object_roles"
   end
 
-  def table_name
-    "object_roles"
-  end
-
-  def my_assigns
-    returning(assigns = {}) do
-      assigns[:migration_action] = "add"
-      assigns[:class_name] = filename
-      assigns[:table_name] = table_name
-      assigns[:attributes] = [Rails::Generator::GeneratedAttribute.new("klazz_name", "string")]
-      assigns[:attributes] << Rails::Generator::GeneratedAttribute.new("object_reference", "integer")
-      assigns[:attributes] << Rails::Generator::GeneratedAttribute.new("user", "references")
-      assigns[:attributes] << Rails::Generator::GeneratedAttribute.new("role", "string")
-    end
-  end
 end
